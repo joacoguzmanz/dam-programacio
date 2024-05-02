8a6cd81a4b2aea40774cae4b31a5bdfb9c26aef7
Joaquin Guzman <joaquingusman77@gmail.com>
2024-05-02 20:52:55 +0200

    practica generar notas

--- src/UF3/Entregas/GenerateFileNotes.java
+++ src/UF3/Entregas/GenerateFileNotes.java
@@ -1,17 +1,30 @@
-package UF3.Ficheros;
+package UF3.Entregas;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Test {
-
+public class GenerateFileNotes {
     public static final String MARCA_FI = "fi";
+    public static final String LINE_TEXT = "%s %s %f";
 
     public static void main(String[] args) {
         try {
-            File f = new File("/Users/joacoguzman/Code/prat/Notes.txt");
-            // f.createNewFile();
+            File f = new File("/Users/joacoguzman/Code/prat/NotaMitja.txt");
+            f.createNewFile();
+
+            PrintStream printStream = new PrintStream(f);
+
+            printStream.println("Maria Almerich 4 3.5 6 -1");
+            printStream.println("Joan Sebastià 4.5 8.5 5 6.5 7 -1");
+            printStream.println("Carme González 6.5 8.75 10 9.5 -1");
+            printStream.println("fi");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+
+        try {
+            File f = new File("/Users/joacoguzman/Code/prat/NotaMitja.txt");
             Scanner lector = new Scanner(f);
             boolean llegir = true;
             while (llegir) {
@@ -21,40 +34,30 @@ public class Test {
                 } else {
                     String cognom = lector.next();
                     System.out.print("Estudiant: " + nom + " " + cognom);
-                    //Noteu com un scanner es pot passar com paràmetre
                     double mitjana = llegirNotes(lector);
                     System.out.println("− Mitjana: " + mitjana);
                 }
             }
-            //Cal tancar el fitxer
             lector.close();
         }
         catch(Exception e) {
-            //Excepció!
             System.out.println("Error llegint estudiants: " + e);
         }
     }
 
-    /** Donat un Scanner en un fitxer quan l’apuntador es troba sobre l’inici
-     * de les notes, s’extreuen i es calcula la mitjana.
-     *
-     * @param lector Scanner a processar
-     * @return Mitjana de notes
-     */
     public static double llegirNotes(Scanner lector) {
         double res = 0;
+        int countNotes = 0;
         try {
-            //Es fan lectures. Cal controlar excepcions també!
-            int numNotes = lector.nextInt();
-            for (int i = 0; i < numNotes; i++) {
-                //S’acumula el valor de les notes
-                res = res + lector.nextDouble();
+            double numNotes = lector.nextDouble();
+            while (numNotes != -1) {
+                res = res + numNotes;
+                countNotes++;
+                numNotes = lector.nextDouble();
             }
-            //Es calcula nota mitjana
-            res = res/numNotes;
+            res = res/countNotes;
         }
         catch (Exception e) {
-            //Excepció!
             System.out.println("Error llegint notes: " + e);
         }
         return res;
